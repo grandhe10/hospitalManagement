c9681b47871a1e38d5cb046f91d62821f97ef410
grandhe10 <sumagrandhe10@gmail.com>
2020-06-23 11:04:14 +0530

    By branch Suma

--- hospitalManagement/src/main/java/com/demo/hospitalManagement/service/AppointmentService.java
+++ hospitalManagement/src/main/java/com/demo/hospitalManagement/service/AppointmentService.java
@@ -9,12 +9,7 @@ import com.demo.hospitalManagement.dto.AppointmentResponseDto;
  */
 public interface AppointmentService {
 	
-	/**
-	 * This method is used to book an appointment
-	 * @param appointmentRequestDto
-	 * @param userId
-	 * @return appointmentResponseDto
-	 */
+	
 	public AppointmentResponseDto bookAnAppointment(AppointmentRequestDto appointmentRequestDto,Long userId);
 	
 
